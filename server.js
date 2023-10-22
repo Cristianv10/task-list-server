072de0eebfbc4b8e67e248a894f8ee3eb90ca194
Cristian Vargas <Cristianjosevp@gmail.com>
2023-10-22 13:32:40 -0400

    project-4

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const tasks = require('./tasks');
+require('dotenv').config(); // Carga las variables de entorno
+const jwt = require('jsonwebtoken');
+
+const validUsers = [
+  { username: 'admin', password: '123' },
+  { username: 'cliente', password: '1234' },
+];
 
 const validateMethod = (req, res, next) => {
   const validMethods = ['GET', 'POST', 'PUT', 'DELETE'];
@@ -9,6 +16,45 @@ const validateMethod = (req, res, next) => {
   }
   next();
 };
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token no proporcionado' });
+  }
+
+  jwt.verify(token, process.env.SECRET_JWT, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ error: 'Token inválido' });
+    }
+
+    req.user = decoded;
+    next();
+  });
+};
+
+
+app.use(express.json());
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  const user = validUsers.find(u => u.username === username && u.password === password);
+
+  if (!user) {
+    return res.status(401).json({ error: 'Credenciales inválidas' });
+  }
+
+  const token = jwt.sign({ username }, process.env.SECRET_JWT, { expiresIn: '1h' });
+
+  res.json({ token });
+});
+
+app.get('/protected', verifyToken, (req, res) => {
+  res.json({ message: 'Ruta protegida', user: req.user });
+});
+
 
 app.use(validateMethod);
 
@@ -26,7 +72,6 @@ app.use('/view', viewRouter);
 app.use('/edit', editRouter);
 
 
-const port = 3000;
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
